42b41c38a8d4b8193c709becb4802edf7fec51a9
root-cause <32650536+root-cause@users.noreply.github.com>
2021-08-24 12:41:07 +0300

    Initial commit

---
+++ MoreCashOptions/Main.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using SimCasino.Modding;
+
+namespace MoreCashOptions
+{
+    [HarmonyPatch(typeof(MainMenuProfile), "Awake")]
+    public class MainMenuProfileAwakePatch
+    {
+        public static bool Prefix(ref List<int> ___CashAmounts)
+        {
+            ___CashAmounts = new List<int> {
+                125_000,
+                250_000,
+                375_000,
+                500_000,
+                625_000,
+                750_000,
+                875_000,
+                1_000_000,
+                1_250_000,
+                1_500_000,
+                1_750_000,
+                2_000_000,
+                2_250_000,
+                2_500_000,
+                3_000_000,
+                3_500_000,
+                4_000_000,
+                4_500_000,
+                5_000_000,
+                6_000_000,
+                7_000_000,
+                8_000_000,
+                9_000_000,
+                10_000_000
+            };
+
+            return true;
+        }
+    }
+
+    public class Main : BaseMod
+    {
+        // Properties
+        public override string InternalName => "root.MoreCashOptions";
+
+        // Fields
+        private Harmony _harmony;
+
+        // Methods
+        public override void OnLoad(GameEnvironment gameState)
+        {
+            _harmony = new Harmony(InternalName);
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# simcasino-starting-cash-options
+
+A mod for [SimCasino](https://store.steampowered.com/app/1158420/SimCasino/) that adds more starting cash options, can be downloaded from the [Steam Workshop page](https://steamcommunity.com/sharedfiles/filedetails/?id=2583596487).
